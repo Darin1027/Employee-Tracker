d14c0944b840621d16832fb0591b3977c9bb4709
DareEnd <114109917+Darin1027@users.noreply.github.com>
2022-12-03 01:02:07 -0700

    added outline of todo in index.js

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,27 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
+    FOREIGN KEY (department),
+    REFERENCES department(id),
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT
+    FOREIGN KEY (role),
+    REFERENCES role(id),
+    ON DELETE SET NULL
 );
--- index.js
+++ index.js
@@ -1,29 +1,52 @@
-// -- As the image illustrates, your schema should contain the following three tables:
+// TODO import modules
 
-// -- department
-//     -- id: INT PRIMARY KEY
-//     -- name: VARCHAR(30) to hold department name
+// TODO function to start the application
 
-// -- role
-//     -- id: INT PRIMARY KEY
-//     -- title: VARCHAR(30) to hold role title
-//     -- salary: DECIMAL to hold role salary
-//     -- department_id: INT to hold reference to department role belongs to
+// TODO-- add a department
+// WHEN I choose to add a department
+// THEN I am prompted to enter the name of the department and that department is added to the database
 
-// -- employee
-//     -- id: INT PRIMARY KEY
-//     -- first_name: VARCHAR(30) to hold employee first name
-//     -- last_name: VARCHAR(30) to hold employee last name
-//     -- role_id: INT to hold reference to employee role
-//     -- manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+// TODO-- add a role
+// WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 
-// -- You might want to use a separate file that contains functions for performing specific SQL queries you'll need to use. A constructor function or class could be helpful for organizing these. You might also want to include a seeds.sql file to pre-populate your database, making the development of individual features much easier.
+// TODO-- add a employee
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 
-// -- Bonus
-// -- Try to add some additional functionality to your application, such as the ability to do the following:
+// TODO-- Update employee role.
+// WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
 
-// -- Update employee managers.
-// -- View employees by manager.
-// -- View employees by department.
-// -- Delete departments, roles, and employees.
-// -- View the total utilized budget of a department—in other words, the combined salaries of all employees in that department.
+// TODO-- View all departments
+// WHEN I choose to view all departments
+// THEN I am presented with a formatted table showing department names and department ids
+
+// TODO-- View all roles
+// WHEN I choose to view all roles
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+
+// TODO-- View all employees
+// WHEN I choose to view all employees
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+
+// BONUS:
+// TODO-- Update employee managers.
+
+// BONUS:
+// TODO-- View employees by manager.
+
+// BONUS:
+// TODO-- View employees by department.
+
+// BONUS:
+// TODO-- Delete departments
+
+// BONUS:
+// TODO-- Delete roles
+
+// BONUS:
+// TODO-- Delete employees
+
+// BONUS:
+// TODO-- View the total utilized budget of a department—in other words, the combined salaries of all employees in that department.
