9b6a89335e9ff83c6340339eea7cc6b7413b4059
DareEnd <114109917+Darin1027@users.noreply.github.com>
2022-12-04 19:51:07 -0700

    updated schema.sql added connection.js

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,14 @@ USE company_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
     FOREIGN KEY (department),
     REFERENCES department(id),
     ON DELETE SET NULL
@@ -20,11 +20,11 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT
-    FOREIGN KEY (role),
-    REFERENCES role(id),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
     ON DELETE SET NULL
 );
